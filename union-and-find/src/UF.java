21f7695d74ca97673dba66c2f7381ba9be6285f9
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2021-03-06 19:49:30 +0700

    update repo

---
+++ union-and-find/src/UF.java
@@ -0,0 +1,5 @@
+public class UF {
+    public UF(int n){}
+    void union(int p, int q){}
+    boolean connected(int p, int q){return true;}
+}
---
+++ union-and-find/src/UFClient.java
@@ -0,0 +1,12 @@
+public class UFClient {
+    public static void main(String[] args){
+        int n = StdIn.readInt();
+        UF uf = new UF(n);
+        while (!StdIn.isEmpty()){
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if ()
+        }
+
+    }
+}
