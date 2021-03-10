d68a70bd03109a43bf8bd7192e0c8c0d9bc8fa14
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2021-03-10 23:25:26 +0700

    update repo

---
+++ binary-search-tree/TreeClient.java
@@ -0,0 +1,12 @@
+
+
+public class TreeClient{
+    public static void main (String[] args){
+        Tree testTree = new Tree();
+        testTree.add(3);
+        testTree.add(5);
+        testTree.add(7);
+        System.out.println(testTree.toString());
+
+    }
+} 
