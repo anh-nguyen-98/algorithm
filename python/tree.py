d2705bf9c94409a6b54999d32fa3a1ff88767346
anh-nguyen-98 <anh.ng.hcm@gmail.com>
2023-03-18 16:04:00 +0700

    add list of depth

--- python/tree.py
+++ python/tree.py
@@ -37,18 +37,47 @@ class BinaryTree:
         result[d].append(node) 
         self.visit_node_at_depth(node.left, d+1, result)
         self.visit_node_at_depth(node.right, d+1, result=result)
+    
+    def list_of_depths_BFS(self):
+        result = []
+        if self.root:
+            children = [self.root]
+            while children:
+                result.append(children)
+                parents = children
+                children = []
+                for p in parents:
+                    if p.left:
+                        children.append(p.left)
+                    if p.right:
+                        children.append(p.right)
+        return result
+                
+    def is_balanced(self):
 
+        return self.get_height(self.root) != -1
+    
+    def get_height(self, node: BinaryTreeNode):
+        if not node:
+            return 0
+        height_left_subtree = self.get_height(node.left)
+        height_right_subtree = self.get_height(node.right)
+        if height_left_subtree == -1 or height_right_subtree == -1:
+            return -1
+        if abs(height_left_subtree - height_right_subtree) > 1:
+            return -1
+        return max(height_right_subtree, height_left_subtree) + 1
 
 root = BinaryTreeNode(6)
 tree = BinaryTree(root=root)
 
 root.left = BinaryTreeNode(3)
-root.right = BinaryTreeNode(10)
+# root.right = BinaryTreeNode(10)
 
 root.left.left = BinaryTreeNode(1)
 root.left.right = BinaryTreeNode(4)
 
-root.right.right = BinaryTreeNode(15)
+# root.right.right = BinaryTreeNode(15)
 
 tree2 = BinaryTree(None)
 
@@ -61,3 +90,4 @@ for d in result:
         d_str += node.to_string() + ' '
     print (d_str)
 
+print(tree.get_height(root))
