d88a45ab6e53359ca5b10dcbc92605e3e0ab5122
anh-nguyen-98 <anh.ng.hcm@gmail.com>
2023-03-25 23:54:52 +0700

    shuffled special string

--- python/string.py
+++ python/string.py
@@ -13,3 +13,39 @@ def is_one_edit_away(s1, s2):
 
     return False
 
+def get_smallest_special_number(s):
+    levels = [
+        {"z": ("zero", 0), "w": ("two", 2), "u": ("four", 4), "x": ("six", 6), "g": ("eight", 8)},
+        {"h": ("three", 3), "f": ("five", 5), "s": ("seven", 7)},
+        {"o": ("one", 1), "i": ("nine", 9)}
+    ] 
+    count = [0 for i in range(26)]
+    for char in s:
+        count[ord(char) - 97] += 1
+
+    result = []
+    for level in levels:
+        for sign in level:
+            sign_id = ord(sign) - 97
+            if count[sign_id] > 0:
+                word = level[sign][0]
+                number = level[sign][1]
+                occurences = count[sign_id]
+                for i in range(occurences):
+                    result.append(number)
+                for char in word:
+                    count[ord(char) - 97] -= occurences
+
+    result = sorted(result)
+    smallest_non_zero = -1
+    for num in result:
+        if num > 0:
+            smallest_non_zero = num
+            break
+    if smallest_non_zero > 0:
+        result.remove(smallest_non_zero)
+        result.insert(0, smallest_non_zero)
+        result =  ''.join(map(str, result))
+    else:
+        result = '0'
+    return result
