dbcc2ebe25297f7e54b7f34ef0d64b64320ea237
anh-nguyen-98 <anh.ng.hcm@gmail.com>
2023-04-07 13:18:10 +0700

    add lowest common ancestor

--- python/tree.py
+++ python/tree.py
@@ -67,27 +67,78 @@ class BinaryTree:
         if abs(height_left_subtree - height_right_subtree) > 1:
             return -1
         return max(height_right_subtree, height_left_subtree) + 1
+    
+    def lowestCommonAncestor(self, p, q):
+        _, LCA = self.findNode(root, [p, q])
+        return LCA.value
 
-root = BinaryTreeNode(6)
+    def findNode(self, root: BinaryTreeNode, unfound_nodes: list):
+        unfound_nodes_count = len(unfound_nodes)
+        if unfound_nodes_count == 2:
+            p = unfound_nodes[0]
+            q = unfound_nodes[1]
+            if root is None:
+                return [False, False], None
+            if root.value == p:
+                # search for q in tree
+                is_found, LCA = self.findNode(root, [q])
+                if is_found[0]:
+                    return [True, True], root
+                return [True, False], None
+            
+            elif root.value == q:
+                is_found, LCA = self.findNode(root, [p])
+                if is_found[0]:
+                    return [True, True], root
+                return [False, True], None
+            else:
+                # search 2 values in left branch
+                is_found, LCA = self.findNode(root.left, unfound_nodes)
+                is_p_found = is_found[0]
+                is_q_found = is_found[1]
+
+                if is_p_found and is_q_found:
+                    return is_found, LCA
+                if is_p_found and not is_q_found:
+                    # search q in right branch
+                    is_found, LCA = self.findNode(root.right, [q])
+                    if is_found[0]:
+                        return [True, True], root
+                    return [True, False], None
+                if not is_p_found and is_q_found:
+                    is_found, LCA = self.findNode(root.right, [p])
+                    if is_found[0]:
+                        return [True, True], root
+                    return [False, True], None
+                return self.findNode(root.right, unfound_nodes)
+            
+        unfound_node = unfound_nodes[0]
+        if root is None:
+            return [False], None
+        if root.value == unfound_node:
+            return [True], None
+        # search node in the left branch
+        is_found, LCA = self.findNode(root.left, unfound_nodes)
+        if is_found[0]:
+            return is_found, LCA
+        return self.findNode(root.right, unfound_nodes)
+
+root = BinaryTreeNode(3)
 tree = BinaryTree(root=root)
 
-root.left = BinaryTreeNode(3)
-# root.right = BinaryTreeNode(10)
+root.left = BinaryTreeNode(5)
+root.right = BinaryTreeNode(1)
 
-root.left.left = BinaryTreeNode(1)
-root.left.right = BinaryTreeNode(4)
+root.left.left = BinaryTreeNode(6)
+root.left.right = BinaryTreeNode(2)
 
+root.left.right.left = BinaryTreeNode(7)
+root.left.right.right = BinaryTreeNode(4)
+
+root.right.left = BinaryTreeNode(0)
+root.right.right = BinaryTreeNode(8)
+
+print (tree.to_string())
+
+print (tree.lowestCommonAncestor(5, 1))
 # root.right.right = BinaryTreeNode(15)
-
-tree2 = BinaryTree(None)
-
-print(tree2.to_string())
-
-result = tree2.list_of_depths()
-for d in result:
-    d_str = ''
-    for node in d:
-        d_str += node.to_string() + ' '
-    print (d_str)
-
-print(tree.get_height(root))
